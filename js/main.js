35d0e23325a2e97a394ce9063ef9e12553ab7687
ahmedYasser166 <ahmed01020269254@gmail.com>
2024-11-23 23:00:33 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var btnSubmit = document.getElementById("btnSubmit");
 var btnVisit = document.getElementById("btnVisit");
 var booksList = [];
 var mycontent;
+
 // =========>localStorage
 if (localStorage.getItem("Books") == null) {
   booksList = [];
@@ -12,12 +13,23 @@ if (localStorage.getItem("Books") == null) {
   booksList = JSON.parse(localStorage.getItem("Books"));
   display();
 }
+
 // =========>add
 function Addbook() {
   if (
-    NameInput.classList.contains("is-valid") &
+    NameInput.classList.contains("is-valid") &&
     UrlInput.classList.contains("is-valid")
   ) {
+    var isDuplicate = booksList.some((book) => book.code === NameInput.value);
+    if (isDuplicate) {
+      Swal.fire({
+        title: "Error!",
+        text: "This name already exists. Please enter a unique name.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+      return;
+    }
     var Books = {
       code: NameInput.value,
       url: UrlInput.value,
@@ -31,27 +43,37 @@ function Addbook() {
       title: "Error!",
       text: "Data is not valid",
       icon: "error",
-      confirmButtonText: "Cool",
+      confirmButtonText: "OK",
     });
   }
-
-  // ============>if valed
 }
 // ============>display
 function display() {
   var cartona = "";
   for (var i = 0; i < booksList.length; i++) {
-    cartona += `<tbody class="text-center" > <td class="fw-bold">${i + 1}</td>
-      <td class="fw-bold">${booksList[i].code}</td>
-      <td class="py-2"><a target="_blank" href="${
-        booksList[i].url
-      }" > <button id="btnVisit" onclick="visitbook( ${i})" class="btn btn-success text-white btn-submit"> <i class="fa-solid fa-eye"></i>Visit</button></a></td>
-      <td class="py-2"><button onclick="deletebook( ${i})" class="btn btn-danger btn-submit"><i class="fa-solid fa-trash"></i> Delete</button>
-      </td></tbody>  `;
+    cartona += `<tbody class="text-center">
+      <tr>
+        <td class="fw-bold">${i + 1}</td>
+        <td class="fw-bold">${booksList[i].code}</td>
+        <td class="py-2">
+          <a target="_blank" href="${booksList[i].url}">
+            <button id="btnVisit" onclick="visitbook(${i})" class="btn btn-success text-white btn-submit">
+              <i class="fa-solid fa-eye"></i>Visit
+            </button>
+          </a>
+        </td>
+        <td class="py-2">
+          <button onclick="deletebook(${i})" class="btn btn-danger btn-submit">
+            <i class="fa-solid fa-trash"></i> Delete
+          </button>
+        </td>
+      </tr>
+    </tbody>`;
   }
   document.getElementById("mycontent").innerHTML = cartona;
 }
-//===============>deleted
+
+//===============>delete
 function deletebook(deletedIndex) {
   booksList.splice(deletedIndex, 1);
   display();
@@ -63,14 +85,14 @@ function clear() {
   NameInput.value = null;
   UrlInput.value = null;
 }
+
 //===============>validatinputs
 function validatinputs(element) {
   var regex = {
     namebook: /^[a-zA-Z0-9-_.]{1,63}$/,
     urlbook: /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/,
   };
-  regex[element.id].test(element.value);
-  if (regex[element.id].test(element.value) === true) {
+  if (regex[element.id].test(element.value)) {
     element.classList.add("is-valid");
     element.classList.remove("is-invalid");
     element.nextElementSibling.classList.add("d-none");
